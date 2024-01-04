e402c8dd77edcc4d37aefc3a26e97ac7f9a1fa38
Lata21 <latab2130@gmail.com>
2024-01-04 13:57:14 +0530

    package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME = "package_creation"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "Lata21"
 AUTHOR_EMAIL = "latab2130@gmail.com"
