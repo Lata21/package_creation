c27308a1f265e84204443dfdedbe0048c4e7c51d
Lata21 <latab2130@gmail.com>
2024-01-04 14:51:04 +0530

    Everything updated

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPEN_E_DOT='-e .'
+
+def get_requirement(file_path:str)->List(str):
+    requirements=[]
+    with open(file_path)as f:
+        requirements=f.readlines()
+        requirements=req.replace("\n","")for req in requirements
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
-   
 
 __version__ = "0.0.4"
 REPO_NAME = "package_creation"
@@ -25,6 +35,7 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=get_requirement("./requirements_dev.txt"),
     )
 
 
